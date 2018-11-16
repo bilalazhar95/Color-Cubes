b8e1c7322589c3b9695f43f7b3b1cedb037de853
Bilal <fagx007@gmail.com>
2018-11-16 17:20:28 +0500

    Messing with cube movement 1

---
+++ Color Cubes/Assets/Cube.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Cube : MonoBehaviour
+{
+    Rigidbody rigidbody=null;
+
+    [SerializeField] private float explosionForce = 10f;
+    [SerializeField] private float explosionRadius = 1f;
+    [SerializeField] private Vector3 blastOffset = new Vector3(0f,-0.5f,0f);
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        rigidbody.AddExplosionForce(explosionForce, transform.position + blastOffset, explosionRadius);
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Boom");
+     
+    }
+}
---
+++ Color Cubes/Assets/Shooter/Rotator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    Transform thisTransform = null;
+
+   [SerializeField] private float rotateSpeed=360f;
+
+    private void Awake()
+    {
+        thisTransform = GetComponent<Transform>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // TODO take rotValue from swipes
+
+        float rotValue = Input.GetAxis("Vertical");
+        transform.localRotation *= Quaternion.AngleAxis(rotValue * rotateSpeed * Time.deltaTime, Vector3.up);
+
+        
+		
+	}
+}
