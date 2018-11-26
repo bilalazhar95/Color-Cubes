845d23c65a87808cb0f92c199979601bcfc9d3c8
Bilal <fagx007@gmail.com>
2018-11-26 16:23:05 +0500

    Changed level design a bit

--- Color Cubes/Assets/Spawner/Spawner.cs
+++ Color Cubes/Assets/Spawner/Spawner.cs
@@ -6,10 +6,10 @@ public class Spawner : MonoBehaviour
 {
     public GameObject[] shootablePrefabs;
 
-    [SerializeField] Transform spawnPosition = null;
+    [SerializeField] Transform[] spawnPositions = null;
     [SerializeField] float initDelay = 3f;
     [SerializeField] float shootableSpawnDelay = 2f;
-    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] Vector2 moveSpeedMinMax = new Vector2(1,7);
 
     float nextSpawnTime = 0f;
 	// Use this for initialization
@@ -31,10 +31,15 @@ public class Spawner : MonoBehaviour
     public void Spawn()
     {
         nextSpawnTime = Time.time + shootableSpawnDelay;
-        int randomPrefab = Random.Range(0, 3);
+
+        int randomPrefab = Random.Range(0, shootablePrefabs.Length);
+        float randomSpeed = Random.Range(moveSpeedMinMax.x,moveSpeedMinMax.y);
+        int randomTransform = Random.Range(0,spawnPositions.Length);
+        Vector3 randomPosition = spawnPositions[randomTransform].position;
+
         IShootable shootable;
-        shootable = Instantiate(shootablePrefabs[randomPrefab], spawnPosition.position, Quaternion.identity).GetComponent<IShootable>();
-        shootable.Move(Vector3.down,moveSpeed, ForceMode.Impulse);
+        shootable = Instantiate(shootablePrefabs[randomPrefab],randomPosition , Quaternion.identity).GetComponent<IShootable>();
+        shootable.Move(Vector3.down,randomSpeed , ForceMode.Impulse);
     
 
         
